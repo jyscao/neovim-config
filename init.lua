649fcd597627ae300d46029c5d520049bcf87a98
Jethro Cao <jethrocao@gmail.com>
2023-09-12 09:38:46 -0600

    Move basic keymaps to separate module

--- init.lua
+++ init.lua
@@ -224,25 +224,7 @@ vim.o.completeopt = 'menuone,noselect'
 vim.o.termguicolors = true
 
 -- [[ Basic Keymaps ]]
-
--- back to normal
-vim.keymap.set({ 'i', 'v', 'o' }, 'jk', '<Esc>', { silent = true })   -- Insert, Replace, Visual, Select, Operator-pending modes
-vim.keymap.set( 'c', 'jk', '<C-c>', { silent = true })    -- Cmdline mode
-vim.keymap.set( 't', 'jk', '<C-\\><C-n>', { silent = true })    -- Terminal mode
-
--- easier window switching
-vim.keymap.set('n', '<C-h>', '<C-w>h')
-vim.keymap.set('n', '<C-j>', '<C-w>j')
-vim.keymap.set('n', '<C-k>', '<C-w>k')
-vim.keymap.set('n', '<C-l>', '<C-w>l')
-
--- Keymaps for better default experience
--- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
--- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+require("keymaps")
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
---
+++ lua/keymaps.lua
@@ -0,0 +1,28 @@
+-- [[ Basic Keymaps ]]
+
+-- back to normal
+vim.keymap.set({ 'i', 'v', 'o' }, 'jk', '<Esc>', { silent = true })   -- Insert, Replace, Visual, Select, Operator-pending modes
+vim.keymap.set( 'c', 'jk', '<C-c>', { silent = true })    -- Cmdline mode
+vim.keymap.set( 't', 'jk', '<C-\\><C-n>', { silent = true })    -- Terminal mode
+
+-- easier window switching
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+
+
+-- -- Diagnostic keymaps
+-- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+-- vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+-- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+-- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
