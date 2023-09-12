ac9b46ec35aebefe0ffdae01b2486d3bce13a0b0
Jethro Cao <jethrocao@gmail.com>
2023-09-12 13:15:32 -0600

    Add treesitter & telescope specs

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,15 @@
+local S = {}
+
+-- function S.init()
+-- end
+
+function S.get_deps()
+  return {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+  }
+end
+
+-- function S.config()
+-- end
+
+return S
