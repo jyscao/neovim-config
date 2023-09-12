48f55d6a9bf0c150cc945e0b261b1222e1e55c99
Jethro Cao <jethrocao@gmail.com>
2023-09-12 09:59:23 -0600

    Move setting global vars (g:) to own module

--- init.lua
+++ init.lua
@@ -1,8 +1,4 @@
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+require("globals")
 
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
