4eda58894cf642e0d5073b6de651ec611ef89aee
Jethro Cao <jethrocao@gmail.com>
2023-09-12 17:43:28 -0600

    Add convenience keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,6 +5,11 @@ vim.keymap.set({ 'i', 'o' }, 'jk', '<Esc>', { silent = true })   -- Insert, Repl
 vim.keymap.set( 'c', 'jk', '<C-c>', { silent = true })    -- Cmdline mode
 vim.keymap.set( 't', 'jk', '<C-\\><C-n>', { silent = true })    -- Terminal mode
 
+-- conveniences
+vim.keymap.set({ "n", "v" }, ";", ":")
+vim.keymap.set("n", "<leader>fs", "<cmd>update<cr>", { silent = true, desc = "save buffer" })
+vim.keymap.set("n", "<leader>wq", "<cmd>x<cr>", { silent = true, desc = "quit current window" })
+
 -- easier window switching
 vim.keymap.set('n', '<C-h>', '<C-w>h')
 vim.keymap.set('n', '<C-j>', '<C-w>j')
@@ -18,11 +23,3 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
-
-
--- -- Diagnostic keymaps
--- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
--- vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
--- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
--- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
