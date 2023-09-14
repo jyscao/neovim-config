5366eac630c6265301968459d6aa12e59be5cb23
Jethro Cao <jethrocao@gmail.com>
2023-09-13 22:17:27 -0600

    Reorganize plugin definition order

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -17,53 +17,45 @@ vim.opt.rtp:prepend(lazypath)
 local _ = require("utils").get_plugin_spec_table
 
 local specifications = {
-  -- Git related plugins
+  -- [[ Git Integrations ]]
   _("tpope/vim-fugitive"),	-- Git wrapper
   _("tpope/vim-rhubarb"),	-- GitHub extension for vim-fugitive
+  _("lewis6991/gitsigns.nvim"),	-- Super fast git decorations in the sign-column
 
+  -- [[ Editing Improvements ]]
+  _("AndrewRadev/linediff.vim"),-- Perform diffs on blocks of code
+  _("numToStr/Comment.nvim"),	-- Comment plugin that supports treesitter
+
+  -- [[ IDE Core ]]
+  _("neovim/nvim-lspconfig"),	-- LSP
+  _("hrsh7th/nvim-cmp"),   	-- Autocompletion
+  _("folke/which-key.nvim"),	-- displays popup of custom keybindings
+  _("nvim-telescope/telescope.nvim"),	-- Ultimate Neovim Fuzzy Funder: find, Filter, preview, pick
+  _("nvim-treesitter/nvim-treesitter"),		-- Treesitter configurations and abstraction layer
+
+  -- [[ Aesthetics ]]
+  _("navarasu/onedark.nvim"),		-- Theme inspired by Atom
+  -- _("folke/tokyonight.nvim"),	-- A clean, dark Neovim theme
+  -- _("catppuccin/nvim"),		-- Soothing pastel theme for (Neo)vim
+  _("nvim-lualine/lualine.nvim"),	-- Set lualine as statusline, `:help lualine.txt`
+
+  -- [[ UI Improvements ]]
+  _("lukas-reineke/indent-blankline.nvim"),	-- Add indentation guides even on blank lines
+
+  -- [[ Terminal Integrations ]]
+  _("willothy/flatten.nvim"),	-- Open files & command output in current neovim instance
+
+
+
+  -- [[ New Plugins -> to be organized]]
+  --
   -- 'tpope/vim-sleuth',	-- Automatically adjusts 'shiftwidth' & 'expandtab' based on heuristics
   _("tpope/vim-rsi"),		-- Readline style insertion
   _("tpope/vim-eunuch"),	-- Helpers for UNIX
   _("tpope/vim-scriptease"),	-- A Vim plugin for Vim plugins
 
-  _("AndrewRadev/linediff.vim"),	-- Perform diffs on blocks of code
-
-  -- NOTE: This is where your plugins related to LSP can be installed.
-  --  The configuration is done below. Search for lspconfig to find it below.
-  _("neovim/nvim-lspconfig"),
-
-  _("hrsh7th/nvim-cmp"),    -- Autocompletion
-
-  -- Useful plugin to show you pending keybinds.
-  _("folke/which-key.nvim"),
-
-  -- Adds git related signs to the gutter, as well as utilities for managing changes
-  _("lewis6991/gitsigns.nvim"),
-
-  _("navarasu/onedark.nvim"),   -- Theme inspired by Atom
-  -- _("folke/tokyonight.nvim"),	-- A clean, dark Neovim theme
-  -- _("catppuccin/nvim"),		-- Soothing pastel theme for (Neo)vim
-
-  _("nvim-lualine/lualine.nvim"),   -- Set lualine as statusline, `:help lualine.txt`
-
-  -- Add indentation guides even on blank lines
-  _("lukas-reineke/indent-blankline.nvim"),   -- See `:help indent_blankline.txt`
-
-  -- "gc" to comment visual regions/lines
-  _("numToStr/Comment.nvim"),
-
-  -- Fuzzy Finder (files, lsp, etc)
-  _("nvim-telescope/telescope.nvim"),
-
-  -- Highlight, edit, and navigate code
-  _("nvim-treesitter/nvim-treesitter"),
-
   -- TODO/FIX: CtrlSpace window's display (status line) is busted
-  _("vim-ctrlspace/vim-ctrlspace"),
-
-
-  _("willothy/flatten.nvim"),	-- Open files & command output in current neovim instance
-
+  _("vim-ctrlspace/vim-ctrlspace"),	-- Workspace Manager: buffers, tabs, files, workspace, bookmarks
 }
 
 local lazy_opts = {
