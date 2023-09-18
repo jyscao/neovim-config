a628c1556271c45e6197ae07aee2c8032f455018
Jethro Cao <jethrocao@gmail.com>
2023-09-18 16:58:38 -0600

    Use inverted mode w/ key 'D' for karen-yank

--- lua/plugins/karen-yank.lua
+++ lua/plugins/karen-yank.lua
@@ -1,6 +1,30 @@
 local S = {}
 
-S.config = true
+S.opts = {
+  mappings = {
+    -- karen controls the use of registers (and probably talks to the manager when things doesn't work as intended)
+    -- map something like `<leader><leader>` if you use the plugin inverted
+    karen = "D",
+    -- false: delete into black hole by default and use registers with karen key
+    -- true: use registers by default and delete into black hole with karen key
+    invert = true,
+    -- disable all if `true` or a table of keymaps [possible values: {"s"|"S"|"d"|"D"|"c"|"C"|"x"|"X"|"p"|"P"|"y"|"Y"}]
+    -- "s"/"S" is not mapped by default, due to it's common utilization for plugins like surround or hop
+    disable = { "s", "S" },
+  },
+  number_regs = {
+    -- use number registers for yanks and cuts
+    enable = true,
+    deduplicate = {
+      -- prevent populating multiple number registers with the same entries
+      enable = true,
+      -- will see `yD` pressed at the beginning of a line as a duplicate of `ydd` pressed in the same line
+      ignore_whitespace = true,
+    },
+  },
+}
+
+-- S.config = true
 
 S.event = "VeryLazy"
 
