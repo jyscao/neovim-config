c3f6b75ae9dcf71dcbcfefddb7bcfb0d2ad4ba4c
Jethro Cao <jethrocao@gmail.com>
2023-09-18 19:20:16 -0600

    Improve formatting in globals.lua

--- lua/globals.lua
+++ lua/globals.lua
@@ -1,5 +1,5 @@
 -----------------------------------------------------------
---            Define basic keymaps for Neovim            --
+--               Explicit Global Variables               --
 -----------------------------------------------------------
 
 -- Set <space> as the leader key; see `:help mapleader`
@@ -7,19 +7,19 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-
-
 -- vim.g.vimsyn_embed = 'l'
 
--- Disable unnecessary remote plugin providers
-vim.g.loaded_perl_provider = 0  -- Disable perl provider
-vim.g.loaded_ruby_provider = 0  -- Disable ruby provider
-vim.g.loaded_node_provider = 0  -- Disable node provider
+
+
+-- [[ Disable unnecessary remote plugin providers ]]
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
 -- vim.g.python3_host_prog = vim.fn.exepath('python3')
 
 
 
--- Disable loading of certain built-in plugins
+-- [[ Disable certain built-in plugins ]]
 
 -- Do not load Netrw; see https://github.com/bling/dotvim/issues/4
 vim.g.loaded_netrw       = 1
@@ -28,15 +28,14 @@ vim.g.loaded_netrwPlugin = 1
 -- Do not load tohtml.vim
 vim.g.loaded_2html_plugin = 1
 
--- Do not load zipPlugin.vim, gzip.vim and tarPlugin.vim (all these plugins are
--- related to checking files inside compressed files)
-vim.g.loaded_zipPlugin = 1
-vim.g.loaded_gzip = 1
+-- Do not load gzip.vim, tarPlugin.vim & zipPlugin.vim, which are for checking files inside compressed archives
+vim.g.loaded_gzip      = 1
 vim.g.loaded_tarPlugin = 1
+vim.g.loaded_zipPlugin = 1
 
 -- Do not load the tutor plugin
 vim.g.loaded_tutor_mode_plugin = 1
 
 -- Do not use builtin matchit.vim nor matchparen.vim since we use sentiment.vim
-vim.g.loaded_matchit = 1
+vim.g.loaded_matchit    = 1
 vim.g.loaded_matchparen = 1
