c716812b00df993ce8dc2bd69b3f34071abaf3dd
Jethro Cao <jethrocao@gmail.com>
2023-09-26 19:54:48 -0600

    Add commented-out register clearing autocmd

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -47,3 +47,15 @@ vim.api.nvim_create_autocmd('BufEnter', {
   group = formatprg_group,
   pattern = '*',
 })
+
+
+-- -- TODO: use below autocmd to clear register on VimEnter?
+-- See: https://vi.stackexchange.com/questions/467/how-can-i-clear-a-register-multiple-registers-completely & https://stackoverflow.com/questions/19430200/how-to-clear-vim-registers-effectively
+-- local vim_init_group = vim.api.nvim_create_augroup('VimInit', { clear = true })
+-- vim.api.nvim_create_autocmd('VimEnter', {
+--   callback = function()
+--     vim.fn.PeekupEmptyRegisters()
+--     vim.cmd.quit()
+--   end,
+--   group = vim_init_group,
+-- })
