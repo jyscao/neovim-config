ee4169d9dffcaa0f5477b1c8497ede51747e92e1
Jethro Cao <jethrocao@gmail.com>
2023-09-26 19:53:28 -0600

    Disable Python remote plugin provider

--- lua/globals.lua
+++ lua/globals.lua
@@ -15,7 +15,9 @@ vim.g.maplocalleader = ' '
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
--- vim.g.python3_host_prog = vim.fn.exepath('python3')
+vim.g.loaded_python_provider = 0
+-- NOTE: if need to use plugins written in Python (e.g. "cjrh/vim-conda", "SirVer/ultisnips"), then
+-- create conda env, install pynvim, hard-code vim.g.python3_host_prog = /conda/env/python3
 
 
 
