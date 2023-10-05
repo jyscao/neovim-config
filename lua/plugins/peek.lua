3bc53792aecdd9aec4ccc97a62401ebbe7e4d3b6
Jethro Cao <jethrocao@gmail.com>
2023-10-05 17:48:53 -0600

    Add descriptions to markdown preview keybinds

--- lua/plugins/peek.lua
+++ lua/plugins/peek.lua
@@ -3,8 +3,8 @@ local S = {}
 S.init = function()
   vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
   vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
-  require("utils.keymap").n('<leader>xm', "<Cmd>PeekOpen<CR>")
-  require("utils.keymap").n('<leader>xM', "<Cmd>PeekClose<CR>")
+  require("utils.keymap").n('<leader>xm', "<Cmd>PeekOpen<CR>",  { desc = "open Markdown preview"  })
+  require("utils.keymap").n('<leader>xM', "<Cmd>PeekClose<CR>", { desc = "close Markdown preview" })
 end
 
 S.config = true
