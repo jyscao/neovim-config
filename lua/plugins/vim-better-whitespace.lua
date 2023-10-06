0011dd4afe653ed10025e29181d5939bff1e3ff3
Jethro Cao <jethrocao@gmail.com>
2023-10-05 18:32:20 -0600

    Add keybind to toggle ws hl to workaround ins-mode hl bug

--- lua/plugins/vim-better-whitespace.lua
+++ lua/plugins/vim-better-whitespace.lua
@@ -1,14 +1,18 @@
 local S = {}
 
 function S.init()
-  vim.g.current_line_whitespace_disabled_soft = true
-  vim.g.better_whitespace_guicolor            = 'cyan'
-  -- vim.g.better_whitespace_ctermcolor       = 'white'
+  vim.g.better_whitespace_guicolor = 'cyan'
+  -- vim.g.better_whitespace_ctermcolor = 'white'
+  -- vim.g.current_line_whitespace_disabled_soft = 1
+  -- vim.g.current_line_whitespace_disabled_hard = 1
 end
 
 function S.config()
   -- set keymaps
-  require("utils.keymap").nx('<leader>xd', "<Cmd>StripWhitespace<CR>")
+  require("utils.keymap").nx('<leader>xw', "<Cmd>ToggleWhitespace<CR>")
+  -- NOTE: this plugin is sometimes buggy, in that it'll highlight whitespaces even
+  -- while in insert mode toggling whitespace higlighting off/on seems to fixes it
+  require("utils.keymap").nx('<leader>xW', "<Cmd>StripWhitespace<CR>")
 end
 
 S.event = "VeryLazy"
