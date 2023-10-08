35121817e4c74b4b76113a8b86482d068e6f0bdd
Jethro Cao <jethrocao@gmail.com>
2023-10-07 23:50:11 -0600

    Set peekup keymaps using utils.keymap to ensure silent map

--- lua/plugins/nvim-peekup.lua
+++ lua/plugins/nvim-peekup.lua
@@ -1,15 +1,17 @@
 local S = {}
 
-function S.init()
-  -- TODO: set keymaps using require("utils.keymap")
-  vim.g.peekup_open = '<leader>""'
-  vim.g.peekup_paste_before = '<leader>"P'
-  vim.g.peekup_paste_after = '<leader>"p'
-  vim.g.peekup_empty_registers = '<leader>"X'
-end
+-- function S.init()
+-- end
 
 function S.config()
   require('nvim-peekup.config').on_keystroke["delay"] = ""
+
+  local keymap = require("utils.keymap")
+  -- set keymaps
+  keymap.n('<leader>""', "<Plug>PeekupOpen")
+  keymap.n('<leader>"p', "<Plug>PeekupPasteAfter")
+  keymap.n('<leader>"P', "<Plug>PeekupPasteBefore")
+  keymap.n('<leader>"X', "<Plug>PeekupEmptyRegisters")
 end
 
 S.event = "VeryLazy"
