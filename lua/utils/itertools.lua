717c5fbab491d3d7b60172823ba499041adb1d72
Jethro Cao <jethrocao@gmail.com>
2023-10-08 23:35:40 -0600

    Add table-printing helper to utils.itertools

--- lua/utils/itertools.lua
+++ lua/utils/itertools.lua
@@ -1,5 +1,35 @@
 local M = {}
 
+-- useful for debugging/viewing table contents (taken from: https://stackoverflow.com/a/41943392/5437918)
+local function stringify_table(tbl, indent)
+  if not indent then indent = 0 end
+  local toprint = string.rep(" ", indent) .. "{\n"
+  indent = indent + 2
+  for k, v in pairs(tbl) do
+    toprint = toprint .. string.rep(" ", indent)
+    if (type(k) == "number") then
+      toprint = toprint .. "[" .. k .. "] = "
+    elseif (type(k) == "string") then
+      toprint = toprint  .. k ..  "= "
+    end
+    if (type(v) == "number") then
+      toprint = toprint .. v .. ",\n"
+    elseif (type(v) == "string") then
+      toprint = toprint .. "\"" .. v .. "\",\n"
+    elseif (type(v) == "table") then
+      toprint = toprint .. stringify_table(v, indent + 2) .. ",\n"
+    else
+      toprint = toprint .. "\"" .. tostring(v) .. "\",\n"
+    end
+  end
+  toprint = toprint .. string.rep(" ", indent-2) .. "}"
+  return toprint
+end
+
+function M.print_tbl(tbl)
+  print(stringify_table(tbl))
+end
+
 function M.list_contains(list, elem)
   for _, v in ipairs(list) do
     if v == elem then
