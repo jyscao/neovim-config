8d14d63e0db0460059cd28c385e3f16327c6fb1c
Jethro Cao <jethrocao@gmail.com>
2023-10-13 00:14:11 -0600

    Chore: alignment

--- lua/plugins/ventana.lua
+++ lua/plugins/ventana.lua
@@ -5,7 +5,7 @@ function S.config()
   -- set keymaps
   keymap.n("<C-w><C-t>", "<Cmd>VentanaTranspose<CR>")
   keymap.n("<C-w><C-f>", "<Cmd>VentanaShift<CR>")
-  keymap.n("<C-w>f", "<Cmd>VentanaShiftMaintainLinear<CR>")
+  keymap.n("<C-w>f",     "<Cmd>VentanaShiftMaintainLinear<CR>")
 end
 
 S.keys = { "<C-w><C-t>", "<C-w><C-f>", "<C-w>f", }
