d05aaaf1833aa109541509bcf4721271682e800a
Jethro Cao <jethrocao@gmail.com>
2023-10-16 14:21:14 -0600

    Update function to set winnr in statusline

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,5 +1,19 @@
 local S = {}
 
+local function set_winnr()
+  local hide_winnr = (
+      vim.fn.winnr('$') == 1 or                     -- tab only has 1 window
+      not vim.bo.modifiable  or                     -- not modifiable buffer (e.g. ctrlspace)
+      vim.api.nvim_win_get_config(0).relative ~= "" -- floating window (e.g. telescope)
+  )
+  local force_show_filetypes = {'help', 'man',}     -- TODO: the 'man' extension overrides our winnr addition, so must extend it
+  local show_winnr = (
+    not hide_winnr or
+    require("utils.itertools").list_contains(force_show_filetypes, vim.bo.filetype)
+  )
+  return show_winnr and vim.fn.winnr() or ""
+end
+
 S.dependencies = {
   { 'nvim-tree/nvim-web-devicons', opt = true },
 }
@@ -12,14 +26,7 @@ S.opts = {
   -- component_separators = '|',    -- default separators:  
   -- section_separators = '',       -- default separators:  
   sections = {
-    lualine_c = {
-      function()
-        -- TODO: exclude floating windows (e.g. telescope, themery) & management windows (e.g. ctrlspace)
-        return vim.fn.winnr('$') == 1 and "" or vim.fn.winnr()
-        -- return #vim.api.nvim_tabpage_list_wins(0) == 1 and "" or vim.fn.winnr()
-      end,
-      'filename',
-    },
+    lualine_c = {set_winnr, 'filename', },
   },
   extensions = {
     'fugitive',
