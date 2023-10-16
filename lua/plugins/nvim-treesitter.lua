3ac56be7ab4bad0bb6ad01fa947c9ebe984ae4ce
Jethro Cao <jethrocao@gmail.com>
2023-10-16 11:31:11 -0600

    Modify treesitter ensure-installed parsers

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -12,7 +12,8 @@ function S.config()
   -- See `:help nvim-treesitter`
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', "markdown", "markdown_inline", },
+    ensure_installed = { 'c', 'cpp', 'elixir', 'heex', 'html', 'javascript', 'lua', "markdown", "markdown_inline", 'nix', 'python', 'scheme', 'vimdoc', 'vim', },
+    -- original list: ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', "markdown", "markdown_inline", },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
