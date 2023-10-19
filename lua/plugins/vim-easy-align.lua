94b2119da8604ae90be0a23b94547a78ba26882b
Jethro Cao <jethrocao@gmail.com>
2023-10-18 22:49:06 -0600

    Modify & add more convenient keymaps for EasyAlign (WIP)

--- lua/plugins/vim-easy-align.lua
+++ lua/plugins/vim-easy-align.lua
@@ -4,8 +4,11 @@ local S = {}
 -- end
 
 function S.config()
+  local keymap = require("utils.keymap")
   -- set keymaps
-  require("utils.keymap").nx('<leader>xa', "<Plug>(EasyAlign)")
+  keymap.nx('<leader>ai', "<Plug>(EasyAlign)", { desc = 'align interactively'})
+  keymap.x('<leader>aa', ':EasyAlign*', { silent = false, desc = 'align on all' })
+  keymap.x('<leader>al', ':EasyAlign-', { silent = false, desc = 'align on last' })
 end
 
 -- S.cmd = "EasyAlign"
