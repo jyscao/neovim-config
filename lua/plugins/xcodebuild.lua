267f6555f8682a36bf4407381250ac1a4e744e57
Jethro Cao <jethrocao@gmail.com>
2025-03-24 21:28:15 -0600

    Add 'XcB' label to xcodebuild keymap descriptions

--- lua/plugins/xcodebuild.lua
+++ lua/plugins/xcodebuild.lua
@@ -15,35 +15,38 @@ S.dependencies = {
 
 function S.config()
   require("xcodebuild").setup({
+    -- put some options here or leave it empty to use default settings
   })
 
   -- set keymaps
   local keymap = require("utils.keymap")
 
-  keymap.n("<leader>xf", "<Cmd>XcodebuildProjectManager<CR>",         { desc = "Show Project Manager Actions" })
+  -- keymap.n("<leader>X",  "<Cmd>XcodebuildPicker<CR>",              { desc = "XcB: Show Actions" })
+  keymap.n("<leader>xf", "<Cmd>XcodebuildProjectManager<CR>",         { desc = "XcB: Show Project Manager Actions" })
 
-  keymap.n("<leader>xb", "<Cmd>XcodebuildBuild<CR>",                  { desc = "Build Project" })
-  keymap.n("<leader>xB", "<Cmd>XcodebuildBuildForTesting<CR>",        { desc = "Build For Testing" })
-  keymap.n("<leader>xr", "<Cmd>XcodebuildBuildRun<CR>",               { desc = "Build & Run Project" })
+  keymap.n("<leader>xb", "<Cmd>XcodebuildBuild<CR>",                  { desc = "XcB: Build Project" })
+  keymap.n("<leader>xB", "<Cmd>XcodebuildBuildForTesting<CR>",        { desc = "XcB: Build For Testing" })
+  keymap.n("<leader>xr", "<Cmd>XcodebuildBuildRun<CR>",               { desc = "XcB: Build & Run Project" })
 
-  keymap.n("<leader>xt", "<Cmd>XcodebuildTest<CR>",                   { desc = "Run Tests" })
-  keymap.x("<leader>xt", "<Cmd>XcodebuildTestSelected<CR>",           { desc = "Run Selected Tests" })
-  keymap.n("<leader>xT", "<Cmd>XcodebuildTestClass<CR>",              { desc = "Run Current Test Class" })
-  keymap.n("<leader>x.", "<Cmd>XcodebuildTestRepeat<CR>",             { desc = "Repeat Last Test Run" })
+  keymap.n("<leader>xt", "<Cmd>XcodebuildTest<CR>",                   { desc = "XcB: Run Tests" })
+  keymap.x("<leader>xt", "<Cmd>XcodebuildTestSelected<CR>",           { desc = "XcB: Run Selected Tests" })
+  keymap.n("<leader>xT", "<Cmd>XcodebuildTestClass<CR>",              { desc = "XcB: Run Current Test Class" })
+  keymap.n("<leader>x.", "<Cmd>XcodebuildTestRepeat<CR>",             { desc = "XcB: Repeat Last Test Run" })
 
-  keymap.n("<leader>xl", "<Cmd>XcodebuildToggleLogs<CR>",             { desc = "Toggle Xcodebuild Logs" })
-  keymap.n("<leader>xc", "<Cmd>XcodebuildToggleCodeCoverage<CR>",     { desc = "Toggle Code Coverage" })
-  keymap.n("<leader>xC", "<Cmd>XcodebuildShowCodeCoverageReport<CR>", { desc = "Show Code Coverage Report" })
-  keymap.n("<leader>xe", "<Cmd>XcodebuildTestExplorerToggle<CR>",     { desc = "Toggle Test Explorer" })
-  keymap.n("<leader>xs", "<Cmd>XcodebuildFailingSnapshots<CR>",       { desc = "Show Failing Snapshots" })
+  keymap.n("<leader>xl", "<Cmd>XcodebuildToggleLogs<CR>",             { desc = "XcB: Toggle Xcodebuild Logs" })
+  keymap.n("<leader>xc", "<Cmd>XcodebuildToggleCodeCoverage<CR>",     { desc = "XcB: Toggle Code Coverage" })
+  keymap.n("<leader>xC", "<Cmd>XcodebuildShowCodeCoverageReport<CR>", { desc = "XcB: Show Code Coverage Report" })
+  keymap.n("<leader>xe", "<Cmd>XcodebuildTestExplorerToggle<CR>",     { desc = "XcB: Toggle Test Explorer" })
+  keymap.n("<leader>xs", "<Cmd>XcodebuildFailingSnapshots<CR>",       { desc = "XcB: Show Failing Snapshots" })
 
-  keymap.n("<leader>xp", "<Cmd>XcodebuildPreviewGenerateAndShow<CR>", { desc = "Generate Preview" })
-  keymap.n("<leader>x<CR>", "<Cmd>XcodebuildPreviewToggle<CR>",       { desc = "Toggle Preview" })
+  keymap.n("<leader>xp", "<Cmd>XcodebuildPreviewGenerateAndShow<CR>", { desc = "XcB: Generate Preview" })
+  keymap.n("<leader>x<CR>", "<Cmd>XcodebuildPreviewToggle<CR>",       { desc = "XcB: Toggle Preview" })
 
-  keymap.n("<leader>xq", "<Cmd>Telescope quickfix<CR>",               { desc = "Show QuickFix List" })
+  -- keymap.n("<leader>xd", "<Cmd>XcodebuildSelectDevice<CR>",        { desc = "XcB: Select Device" })
+  keymap.n("<leader>xq", "<Cmd>Telescope quickfix<CR>",               { desc = "XcB: Show QuickFix List" })
 
-  keymap.n("<leader>xx", "<Cmd>XcodebuildQuickfixLine<CR>",           { desc = "Quickfix Line" })
-  keymap.n("<leader>xa", "<Cmd>XcodebuildCodeActions<CR>",            { desc = "Show Code Actions" })
+  keymap.n("<leader>xx", "<Cmd>XcodebuildQuickfixLine<CR>",           { desc = "XcB: Quickfix Line" })
+  keymap.n("<leader>xa", "<Cmd>XcodebuildCodeActions<CR>",            { desc = "XcB: Show Code Actions" })
 end
 
 S.ft = "swift"
