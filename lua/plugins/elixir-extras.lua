9b36eb08ef995d401979abf44456820e788c3296
Jethro Cao <jethrocao@gmail.com>
2025-03-26 16:56:10 -0600

    Update elixir-extras plugin
    * keep only the multiple-clause function marker

--- lua/plugins/elixir-extras.lua
+++ lua/plugins/elixir-extras.lua
@@ -1,11 +1,16 @@
 local S = {}
 
 S.dependencies = {
-  "nvim-telescope/telescope.nvim",
+  -- "nvim-telescope/telescope.nvim",
 }
 
 function S.config()
-  require("utils.keymap").n("<leader>se", "<Cmd>lua require('elixir-extras').elixir_view_docs({include_mix_libs=true})<CR>")
+  -- FIXME: the `texthl=TSFunction` doesn't appear to work; OR
+  -- have it not conflict w/ gitsigns, e.g. change marker colors to be the same as that of the signs on the same line
+  require("elixir-extras").setup_multiple_clause_gutter()
+
+  -- require("utils.keymap").n("<leader>se", "<Cmd>lua require('elixir-extras').elixir_view_docs({include_mix_libs=true})<CR>")
+  -- require("elixir-extras").module_complete()
 end
 
 S.ft = {"elixir", "heex", }
